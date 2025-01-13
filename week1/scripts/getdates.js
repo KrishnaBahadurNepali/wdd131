00d29bba1eb204bfb1858375a6bf704c61315444
KrishnaBahadurNepali <krishnabahadur.nepalip90x@gmail.com>
2025-01-13 17:24:47 +0545

    date and time

--- week1/scripts/getdates.js
+++ week1/scripts/getdates.js
@@ -1,41 +1,6 @@
-// Function to get and display the current date and time
-  function displayCurrentDate() {
-  //const now = new Date();
-  //const dateString = now.toLocaleString(); // Localized date and time format
-  //const dateElement = document.getElementById("current-date").innerHTML=date();
-  //dateElement.textContent = "Current Date and Time: ${dateElement}"; 
-  document.getElementById("current-date").innerHTML=date();
+function displayDate() {
+  const today=new Date();
+  const formatDate=today.toLocaleDateString('en-US', {  year:'numeric',month:'long',day:'numeric'});
+  document.getElementById("date-display").textContent=formatDate
 }
-document.getElementById("para").style
-// Call the function when the page loads
-window.onload = displayCurrentDate;
-
-
-document.getElementById('flag').addEventListener('mouseover', function () {
-  this.style.animationDuration = '1s'; // Increase flutter speed
-});
-
-document.getElementById('flag').addEventListener('mouseout', function () {
-  this.style.animationDuration = '2s'; // Return to normal speed
-});
-
-const dateElement = document.getElementById("date-display"); // Assume this is your element
-const currentDate = new Date();
-const dateString = currentDate.toLocaleString(); // Format date and time
-dateElement.textContent = 'Current Date and Time: ${dateString}';
-
-
-/* For counting image slide */
-const images = document.querySelectorAll('.image-cont img');
-
-document.addEventListener('mouseover', () => {
-    images.forEach((img) => {
-        img.style.animationPlayState = 'paused'; // Pause animation
-    });
-});
-
-document.addEventListener('mouseout', () => {
-    images.forEach((img) => {
-        img.style.animationPlayState = 'running'; // Resume animation
-    });
-});
+displayDate();
