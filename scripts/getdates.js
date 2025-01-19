62449247cbd297ee5d011a939a869d7b791d6545
KrishnaBahadurNepali <krishnabahadur.nepalip90x@gmail.com>
2025-01-19 10:41:07 +0545

    Color check

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,5 @@
-function displayDate() {
-  const today=new Date();
-  const formatDate=today.toLocaleDateString('en-US', {  year:'numeric',month:'long',day:'numeric'});
-  document.getElementById("date-display").textContent=formatDate
-}
-displayDate();
+const lastModified=new Date(document.lastModified);
+const options={year:"numeric", month:"long",day:"numeric"};
+const formattedDate=lastModified.toLocaleDateString("en-US","options");
+document.addEventListener("DOMContentLoaded", function(){
+document.getElementById("date-display").textContent=formattedDate});
