6adfc6af104b68d0d98a2c5bfcfe939bed849d53
KrishnaBahadurNepali <krishnabahadur.nepalip90x@gmail.com>
2025-03-05 12:28:00 +0545

    java script added

--- week1/scripts/getdates.js
+++ week1/scripts/getdates.js
@@ -3,3 +3,4 @@ const options={year:"numeric", month:"long",day:"numeric"};
 const formattedDate=lastModified.toLocaleDateString("en-US","options");
 document.addEventListener("DOMContentLoaded", function(){
 document.getElementById("date-display").textContent=formattedDate});
+//This java script is use for making dynamic web page
